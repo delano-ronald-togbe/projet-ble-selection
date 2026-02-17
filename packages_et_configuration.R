21eb223365afd34f5356b14cfc2051ef6868a500
delano-ronald-togbe <delanodray@gmail.com>
2026-02-17 16:15:17 +0100

    update

--- packages_et_configuration.R
+++ packages_et_configuration.R
@@ -10,10 +10,19 @@ install_if_missing <- function(p) {
 }
 invisible(lapply(required_packages, install_if_missing))
 
-# Créer une structure de projet
-# dir.create("projet_ble_selection")
-# setwd("projet_ble_selection")
-# dir.create("data")
-# dir.create("scripts")
-# dir.create("outputs")
-# dir.create("reports")
+# Création des défférents sous répertoires du projet
+
+# projet_ble_selection/
+# ├── data/                     # Données brutes et transformées
+# ├── scripts/
+# │ ├── 01_acquisition.R
+# │ ├── 02_preprocessing.R
+# │ ├── 03_gwas.R
+# │ ├── 04_prediction_benchmark.R
+# │ ├── 05_visualisation.R
+# │ └── 06_shiny_app/
+# │     ├── app.R
+# │     └── Dockerfile
+# ├── outputs/                   # Graphiques et résultats
+# ├── reports/                   # Rapport Rmd
+# └── renv.lock                  # Gestion des versions (optionnel)
